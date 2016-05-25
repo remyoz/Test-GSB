3b9e8a1a08b37f09774ccea73cd9e87b4b94973a
qwerty <qwerty@remyPCNew>
2016-05-25 23:53:19 +0200

    new commit

---
+++ newEmptyPHP.php
@@ -0,0 +1,10 @@
+<?php
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+c:\> php -r "file_put_contents('symfony', file_get_contents('https://symfony.com/installer'));
+    
+    ?>
